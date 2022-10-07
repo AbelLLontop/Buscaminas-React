2093097c9f30b89a7549125f94b13b90d388f3d7
AbelLlontopMeza <a.llontopmeza@gmail.com>
2022-10-06 22:05:10 -0500

    disable react-devtolls

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App';
 import './index.css'
 
+import {disableReactDevTools} from '@fvilers/disable-react-devtools';
+
+disableReactDevTools();
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 
     <App />
